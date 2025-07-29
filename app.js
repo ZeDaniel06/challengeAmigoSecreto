ef52f8bd829e897b814fa68039c28a8a7ad73857
ZeDaniel06 <silvadan752@gmail.com>
2025-07-29 20:31:16 -0300

    Função de sortear amigo; Projeto completo como solicitado!

--- app.js
+++ app.js
@@ -19,4 +19,15 @@ function listarAmigos(){
     }
 }
 
+function sortearAmigo(){
+    if(amigos.length == 0){
+        alert("Você deve adicionar algum amigo na lista antes de sortear!");
+    }else{
+        let numeroAleatorio = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[numeroAleatorio];
+        //<ul id="resultado" class="result-list" aria-live="polite"></ul>
+        document.getElementById("resultado").innerHTML = `<ul id="resultado" class="result-list" aria-live="polite">O teu amigo secreto é ${amigoSorteado}</ul>`;
+        
+    }
+}
 let amigos = [];
