6d054c379e0b9b6e53d84a98372a338acef867ad
ZeDaniel06 <silvadan752@gmail.com>
2025-07-29 20:20:38 -0300

    Função Adicionar Amigo em Listas; Função ListarAmigos;

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+function adicionarAmigo(){
+    let textoEscrito = document.getElementById("amigo").value;
+    if (textoEscrito == ''){
+        alert('Por favor, insira um nome.');
+    }else{
+        amigos.push(textoEscrito);
+        document.getElementById("amigo").value = '';
+        listarAmigos();
+    }
+}
+
+function listarAmigos(){
+    //<ul id="listaAmigos" class="name-list" aria-labelledby="listaAmigos" role="list"></ul>
+    let listaHtml = document.getElementById("listaAmigos");
+    listaHtml.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++){
+        listaHtml.innerHTML += `<ul id="listaAmigos" class="name-list" aria-labelledby="listaAmigos" role="list">${amigos[i]}</ul>`;
+    }
+}
+
+let amigos = [];
